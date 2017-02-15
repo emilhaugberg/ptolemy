2b1b8fa7434f2b6dba6ab54cb69a2a8153683ea8
Emil Haugberg <emilveen@gmail.com>
2017-02-15 23:17:30 +0100

    Moare code cleanup.

--- src/Main.purs
+++ src/Main.purs
@@ -1,13 +1,21 @@
 module Main where
 
 import Config.Main (circle, coordFromAngle, height, speed, startingAngle, triangle, width)
+import Config.Types (Angle)
 import Control.Monad.Eff (Eff, foreachE)
 import Control.Monad.Eff.Ref (REF, modifyRef, newRef, readRef)
 import Control.Monad.Eff.Timer (TIMER, setInterval)
 import Data.Maybe (Maybe(..))
-import Graphics.Canvas (CANVAS, clearRect, getCanvasElementById, getContext2D)
+import Graphics.Canvas (CANVAS, Context2D, clearRect, getCanvasElementById, getContext2D)
 import Graphics.Draw (drawCircle, drawDot, drawTriangle, drawLinesToDot, drawLength)
 import Prelude (Unit, bind, void, ($), (+))
+
+draw :: forall e. Context2D -> Angle -> Eff (canvas :: CANVAS | e) Unit
+draw ctx ang = void do
+  drawCircle circle ctx
+  drawTriangle triangle ctx
+  drawDot (coordFromAngle ang) ctx
+  drawLength (coordFromAngle ang) triangle ctx
 
 main :: forall e. (Partial) => Eff (ref :: REF, timer :: TIMER, canvas :: CANVAS | e) Unit
 main = void do
@@ -21,9 +29,5 @@ main = void do
     clearRect ctx {x: 0.0, y: 0.0, w: width, h: height}
     modifyRef angle ((+) speed)
     angle' <- readRef angle
-    drawCircle circle ctx
-    drawTriangle triangle ctx
-    drawDot (coordFromAngle angle') ctx
-    drawLength (coordFromAngle angle') triangle ctx
-
+    draw ctx angle'
     foreachE triangleDots \tp -> void $ drawLinesToDot (coordFromAngle angle') tp ctx
